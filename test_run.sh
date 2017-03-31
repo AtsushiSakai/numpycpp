370e28fb398f9018fdd78e9218efc9ff419deb51
AtsushiSakai <asakai.amsl+github@gmail.com>
2017-03-31 15:30:25 -0700

    add eigen path

--- test_run.sh
+++ test_run.sh
@@ -2,5 +2,5 @@ echo "Test starts..."
 # clang -c set.c
 # clang++ -lgtest -std=c++11 -pthread -g -Wall -Wextra -o unit_test.o -c test.cpp
 # clang++ -std=c++11 -g -Wall -Wextra -o unit_test set.o unit_test.o -lgtest -pthread
-g++ -pthread ./numpycppTest.cpp -lpthread -lgtest_main -lgtest -std=c++0x && ./a.out
+g++ -pthread ./numpycppTest.cpp -I./eigen/ -lpthread -lgtest_main -lgtest -std=c++0x && ./a.out
 exit $result
